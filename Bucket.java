78ac1f3513e521b54a101521568f40b6cd717478
natan-alex <natancarmo321@gmail.com>
2021-04-29 12:04:27 -0300

    Inicio da criação do índices: arquivos iniciais

---
+++ Bucket.java
@@ -0,0 +1,22 @@
+package trabalho_aed_prontuario;
+
+import java.util.ArrayList;
+
+public class Bucket {
+    // Cada bucket contém o valor de profundidade local (p’) seguido por n entradas
+    private int profundidade_local;
+    private int n; // quantidade máxima de registros do bucket
+    private int ocupacao; // quantidade atual de registros do bucket
+    private ArrayList<RegistroDoIndice<Integer>> registros;
+
+    public Bucket(int profundidade_local, int n) {
+        this.profundidade_local = profundidade_local;
+        this.n = n;
+        ocupacao = 0;
+        registros = new ArrayList<RegistroDoIndice<Integer>>(n);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(registros.get(i));
+        }
+    }
+}
