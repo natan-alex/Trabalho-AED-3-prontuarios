ee595a6104a743245880cfc98782a69d4d147785
natan-alex <natancarmo321@gmail.com>
2021-04-30 16:41:12 -0300

    Ignorar arquivos intellij e renomear RegistroDoIndice

--- TesteBucket.java
+++ TesteBucket.java
@@ -2,7 +2,8 @@ package trabalho_aed_prontuario;
 
 public class TesteBucket {
     public static void main(String[] args) {
-        Bucket bucket = new Bucket(0, 5);
+        Bucket<Integer> bucket = new Bucket<Integer>(5, 0);
+        System.out.println( bucket.adicionarRegistro( new RegistroDoBucket<Integer>(10, 100) ) );
     }
 }
 
