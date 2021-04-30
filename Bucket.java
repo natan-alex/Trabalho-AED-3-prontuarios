6b1871e877f67b3980a9d1f73cc5ee1dcb8f6852
natan-alex <natancarmo321@gmail.com>
2021-04-30 16:39:02 -0300

    Adição de getters e setters e generics a Bucket.java

--- Bucket.java
+++ Bucket.java
@@ -2,17 +2,41 @@ package trabalho_aed_prontuario;
 
 import java.util.ArrayList;
 
-public class Bucket {
+public class Bucket<T> {
     // Cada bucket contém o valor de profundidade local (p’) seguido por n entradas
     private int profundidade_local;
     private int n; // quantidade máxima de registros do bucket
-    private int ocupacao; // quantidade atual de registros do bucket
-    private ArrayList<RegistroDoIndice<Integer>> registros;
+    private ArrayList<RegistroDoBucket<T>> registros;
 
-    public Bucket(int profundidade_local, int n) {
-        this.profundidade_local = profundidade_local;
+    public Bucket(int n) {
         this.n = n;
-        ocupacao = 0;
-        registros = new ArrayList<RegistroDoIndice<Integer>>(n);
+        profundidade_local = 0;
+        registros = new ArrayList<RegistroDoBucket<T>>(n);
+    }
+
+    public Bucket(int n, int profundidade_local) {
+        this.n = n;
+        this.profundidade_local = profundidade_local;
+        registros = new ArrayList<RegistroDoBucket<T>>(n);
+    }
+
+    public boolean adicionarRegistro(RegistroDoBucket<T> registro) {
+        // capacidade total do bucket já ocupada
+        if (registros.size() == n) {
+            return false;
+        }
+
+        registros.add(registro);
+        return true;
+    }
+
+    public boolean removerRegistro(T chave_do_registro) {
+        for (RegistroDoBucket<T> registro : registros) {
+            if (registro.getChave() == chave_do_registro) {
+                registro.setIsLapide(true);
+                return true;
+            }
+        }
+        return false;
     }
 }
