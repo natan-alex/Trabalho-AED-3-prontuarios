30635947bba6fd8412252815974d2bae6c9782b6
natan-alex <natancarmo321@gmail.com>
2021-04-30 17:23:09 -0300

    Remoção de lógica desnecessária em Bucket.java

--- Bucket.java
+++ Bucket.java
@@ -1,42 +1,41 @@
 package trabalho_aed_prontuario;
 
-import java.util.ArrayList;
-
 public class Bucket<T> {
     // Cada bucket contém o valor de profundidade local (p’) seguido por n entradas
     private int profundidade_local;
     private int n; // quantidade máxima de registros do bucket
-    private ArrayList<RegistroDoBucket<T>> registros;
+    private int ocupacao;
 
     public Bucket(int n) {
         this.n = n;
         profundidade_local = 0;
-        registros = new ArrayList<RegistroDoBucket<T>>(n);
+        ocupacao = 0;
     }
 
     public Bucket(int n, int profundidade_local) {
         this.n = n;
         this.profundidade_local = profundidade_local;
-        registros = new ArrayList<RegistroDoBucket<T>>(n);
+        ocupacao = 0;
+    }
+
+    public int getProfundidadeLocal() {
+        return profundidade_local;
+    }
+
+    public void setProfundidadeLocal(int profundidade_local) {
+        this.profundidade_local = profundidade_local;
+    }
+
+    public int getN() {
+        return n;
     }
 
     public boolean adicionarRegistro(RegistroDoBucket<T> registro) {
         // capacidade total do bucket já ocupada
-        if (registros.size() == n) {
-            return false;
-        }
-
-        registros.add(registro);
         return true;
     }
 
     public boolean removerRegistro(T chave_do_registro) {
-        for (RegistroDoBucket<T> registro : registros) {
-            if (registro.getChave() == chave_do_registro) {
-                registro.setIsLapide(true);
-                return true;
-            }
-        }
-        return false;
+        return true;
     }
 }
