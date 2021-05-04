7c1760b6ba81c827dfea51833c78d41de8fd8c63
natan-alex <natancarmo321@gmail.com>
2021-05-04 15:49:09 -0300

    Renomear TesteBucket para TesteIndice

--- TesteBucket.java
+++
@@ -1,9 +0,0 @@
-package trabalho_aed_prontuario;
-
-public class TesteBucket {
-    public static void main(String[] args) {
-        Bucket<Integer> bucket = new Bucket<Integer>(5, 0);
-        System.out.println( bucket.adicionarRegistro( new RegistroDoBucket<Integer>(10, 100) ) );
-    }
-}
-
