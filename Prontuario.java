c35fb7abb8d5cab472eebc7a578e7dc2a1159f1e
natan-alex <natancarmo321@gmail.com>
2021-05-05 16:31:39 -0300

    Implementação dos métodos toByteArray e fromByteArray e novo construtor

--- Prontuario.java
+++ Prontuario.java
@@ -1,4 +1,4 @@
-package trabalho_aed_prontuario;
+package trabalho_aed_prontuario.mestre;
 
 import java.time.LocalDate;
 
@@ -13,41 +13,49 @@ import trabalho_aed_prontuario.indice.Serializavel;
 public class Prontuario implements Serializavel {
     // nome, data de nascimento, sexo e uma área de m caracteres/bytes para anotações do médico
     private static final byte MAX_SIZE_NOME = (byte) 50;
-    private static final short MAX_SIZE_ANOTACOES = 255;
-    private StringBuilder nome;
+    private String nome;
     private LocalDate data; 
     private char sexo;
-    private StringBuilder anotacoes;
+    private String anotacoes;
+    private short tam_anotacoes;
 
-    public Prontuario(String nome, LocalDate data, char sexo, String anotacoes) {
+    public Prontuario(String nome, LocalDate data, char sexo, short tam_anotacoes) {
+        setNome(nome);
         this.data = data;
         this.sexo = sexo;
+        this.tam_anotacoes = tam_anotacoes;
+        setAnotacoes("");
+    }
+
+    public Prontuario(String nome, LocalDate data, char sexo, short tam_anotacoes, String anotacoes) {
         setNome(nome);
+        this.data = data;
+        this.sexo = sexo;
+        this.tam_anotacoes = tam_anotacoes;
         setAnotacoes(anotacoes);
-        System.out.println("nome: " + this.nome + " tam: " + this.nome.length());
-        System.out.println("anotacoes: " + this.anotacoes + " tam: " + this.anotacoes.length() );
     }
 
     public void setNome(String nome) {
-        this.nome = new StringBuilder(MAX_SIZE_NOME);
+        // se o argumento tiver tamanho menor que o tamanho máximo
+        // completar com espaços, senão, limitar os caracteres até
+        // a quantidade máxima, "cortando "a string
         if (nome.length() < MAX_SIZE_NOME) {
-            System.out.println("menor");
-            this.nome.append( String.format("%-"+MAX_SIZE_NOME+"s", nome) );
+            this.nome = String.format("%-"+MAX_SIZE_NOME+"s", nome); 
         } else {
-            this.nome.append(nome);
-            this.nome.setLength(MAX_SIZE_NOME);
+            this.nome = nome.substring(0, MAX_SIZE_NOME);
         }
+        System.out.println("nome: " + this.nome + " tam: " + this.nome.length());
     }
 
     public void setAnotacoes(String anotacoes) {
-        this.anotacoes = new StringBuilder(MAX_SIZE_ANOTACOES);
-        if (anotacoes.length() < MAX_SIZE_ANOTACOES) {
-            System.out.println("menor");
-            this.anotacoes.append( String.format("%-"+MAX_SIZE_ANOTACOES+"s", anotacoes) );
+        // faz o mesmo que a setNome com relação ao tamanho e corte
+        // da string que vier como argumento
+        if (anotacoes.length() < tam_anotacoes) {
+            this.anotacoes = String.format("%-"+tam_anotacoes+"s", anotacoes);
         } else {
-            this.anotacoes.append(anotacoes);
-            this.anotacoes.setLength(MAX_SIZE_ANOTACOES);
+            this.anotacoes = anotacoes.substring(0, tam_anotacoes);
         }
+        System.out.println("anotacoes: " + this.anotacoes + " tam: " + this.anotacoes.length());
     }
 
     @Override
@@ -55,26 +63,41 @@ public class Prontuario implements Serializavel {
         return "Prontuario: (nome = " + nome + ", data = " + data + ", sexo = " + sexo + ", anotacoes = " + anotacoes + ")";
     }
 
+    // retorna um array de bytes com os valores dos atributos
     public byte[] toByteArray() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
 
-        // try {
-        //     // dos.write
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
+        try {
+            dos.writeUTF(nome);
+            // dos.writeShort( (short) data.getYear() );
+            // dos.writeByte( (byte) data.getMonthValue() );
+            // dos.writeByte( (byte) data.getDayOfMonth() );
+            dos.writeShort( data.getYear() );
+            dos.writeByte( data.getMonthValue() );
+            dos.writeByte( data.getDayOfMonth() );
+            dos.writeChar(sexo);
+            dos.writeUTF(anotacoes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return baos.toByteArray();
     }
 
+    // preenche os valores dos atributos com base nos bytes
+    // vindos de um array de bytes
     public void fromByteArray(byte[] data) {
-        
-        // try {
-        //     ByteArrayInputStream bais = new ByteArrayInputStream(data);
-        //     DataInputStream dis = new DataInputStream(bais);
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
+        ByteArrayInputStream bais = new ByteArrayInputStream(data);
+        DataInputStream dis = new DataInputStream(bais);
+
+        try {
+            this.nome = dis.readUTF();
+            this.data = LocalDate.of( dis.readShort(), dis.readByte(), dis.readByte() );
+            this.sexo = dis.readChar();
+            setAnotacoes( dis.readUTF() );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
