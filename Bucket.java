52f4b0738fdea013d97dc19a31c391151f69f4d6
natan-alex <natancarmo321@gmail.com>
2021-05-19 12:10:37 -0300

    Add comentários, método obterStatusDeUmaNovaInsercao e alguns ajustes

--- Bucket.java
+++ Bucket.java
@@ -21,10 +21,13 @@ public class Bucket {
     public Bucket(int profundidade_local, int tam_bucket) {
         if (profundidade_local > 0)
             this.profundidade_local = profundidade_local;
+
         if (tam_bucket > 0)
             this.tam_bucket = tam_bucket;
+
         this.ocupacao = 0; // ocupação inicial é 0
         registros = new RegistroDoBucket[tam_bucket];
+
         // inicializar registros do bucket com valores
         // default
         for (int i = 0; i < this.tam_bucket; i++) {
@@ -35,38 +38,58 @@ public class Bucket {
     public Bucket(int tam_bucket, byte[] bucket_em_bytes) {
         if (tam_bucket > 0)
             this.tam_bucket = tam_bucket;
+
+        registros = new RegistroDoBucket[tam_bucket];
+
         try (ByteArrayInputStream bais = new ByteArrayInputStream(bucket_em_bytes);
              DataInputStream dis = new DataInputStream(bais);
             ) {
+            // para os bytes de um registro
             byte[] registro_em_bytes = new byte[SIZEOF_REGISTRO_BUCKET];
+
             // ler metadados do bucket
             this.profundidade_local = dis.readInt();
             this.ocupacao = dis.readInt();
+
             // ler registros do bucket
             for (int i = 0; i < ocupacao; i++) {
                 dis.read(registro_em_bytes);
                 registros[i] = new RegistroDoBucket(registro_em_bytes);
+            }
+
+            // inicializar restante dos registros do bucket
+            // com valores default
+            for (int i = ocupacao; i < this.tam_bucket; i++) {
+                registros[i] = new RegistroDoBucket();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public Bucket(int tam_bucket, int profundidade_local, int ocupacao, byte[] registros_do_bucket_em_bytes) {
-        if (tam_bucket > 0)
-            this.tam_bucket = tam_bucket;
-        if (profundidade_local > 0)
-            this.profundidade_local = profundidade_local;
-        if (ocupacao > 0 && ocupacao <= tam_bucket)
-            this.ocupacao = ocupacao;
+    // criar novo bucket a partir de informações vindas do arquivo
+    // de índices
+    protected Bucket(int tam_bucket, int profundidade_local, int ocupacao, byte[] registros_do_bucket_em_bytes) {
+        this.tam_bucket = tam_bucket;
+        this.profundidade_local = profundidade_local;
+        this.ocupacao = ocupacao;
+        registros = new RegistroDoBucket[tam_bucket];
+
         try (ByteArrayInputStream bais = new ByteArrayInputStream(registros_do_bucket_em_bytes);
              DataInputStream dis = new DataInputStream(bais);
             ) {
             byte[] registro_em_bytes = new byte[SIZEOF_REGISTRO_BUCKET];
+
             // ler registros do bucket
             for (int i = 0; i < ocupacao; i++) {
                 dis.read(registro_em_bytes);
                 registros[i] = new RegistroDoBucket(registro_em_bytes);
+            }
+
+            // inicializar restante dos registros do bucket
+            // com valores default
+            for (int i = ocupacao; i < this.tam_bucket; i++) {
+                registros[i] = new RegistroDoBucket();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -89,47 +112,40 @@ public class Bucket {
         this.ocupacao = ocupacao;
     }
 
-    // obter o tamanho total em bytes de um bucket qualquer
-    // quando cheio
-    public int get_tamanho_total_em_bytes_de_um_bucket() {
-        // 4 para a profundidade_local, 4 para a ocupacao
-        // e os registros
-        return 4 + 4 + (tam_bucket * SIZEOF_REGISTRO_BUCKET);
+    public RegistroDoBucket[] getRegistrosDoBucket() {
+        return registros;
     }
 
-    // inserir um novo registro no array de registros;
-    // retorna -2 caso o registro seja inválido,
-    // -1 em caso de necessidade de duplicar o bucket,
+    // retorna -1 em caso de necessidade de duplicar o bucket,
     // 0 em caso de tudo ok e
     // > 0 (profundidade com que o novo bucket deve 
     // ser criado) caso seja necessário criar novo bucket 
     // e rearranjar as chaves do novo bucket e do bucket atual
-    public int inserir_registro(RegistroDoBucket registro, int profundidade_global) {
-        if (registro == null) {
-            return -2;
-        }
+    public static int obterStatusDeUmaNovaInsercao(int ocupacao, int p_local, int p_global, int tam_bucket) {
         if (ocupacao == tam_bucket) {
-            if (profundidade_local == profundidade_global) {
+            if (p_local == p_global) {
+                // necessário duplicar o bucket
                 System.out.println("necessário duplicar dir");
                 return -1;
             } else {
+                // necessário criar novo bucket e rearranjar 
+                // os registros do bucket em questão
                 System.out.println("necessário criar novo bucket!");
                 System.out.println("necessário rearranjar chaves!");
 
-                return ++profundidade_local;
+                return ++p_local;
             }
         } else {
-            // inserir registro no fim do bucket
-            // e atualizar a ocupacao
-            registros[ocupacao++] = registro;
+            // tudo ok, inserção pode ser feita
+            return 0;
         }
-        return 0;
     }
 
     // serializar o bucket no seu estado atual:
     // insere a profundidade_local, a ocupacao
-    // e os ocupacao registros existentes no bucket
-    public byte[] serializar_bucket() {
+    // e os registros do bucket(todos, incluindo 
+    // os que foram inicializados com valores default)
+    public byte[] serializarBucket() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
 
@@ -138,7 +154,7 @@ public class Bucket {
             dos.writeInt(profundidade_local);
             dos.writeInt(ocupacao);
             // escrever registros do bucket
-            for (int i = 0; i < ocupacao; i++) {
+            for (int i = 0; i < tam_bucket; i++) {
                 dos.write( registros[i].toByteArray() );
             }
             dos.flush();
@@ -155,7 +171,7 @@ public class Bucket {
     // em um array de bytes
     // recebe o número do registro no bucket(1 para o primeiro,
     // 2 para o segundo registro, etc)
-    public byte[] serializar_registro(int num_registro_no_bucket) {
+    public byte[] serializarRegistro(int num_registro_no_bucket) {
         // se número do registro inválido, não há o que serializar
         if (num_registro_no_bucket <= 0 || num_registro_no_bucket > tam_bucket)
             return null;
