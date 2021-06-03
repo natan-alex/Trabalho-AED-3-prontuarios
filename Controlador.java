4ca050fd0a32969117fad4e1c6eca2f7bc6f2f10
natan-alex <natancarmo321@gmail.com>
2021-06-03 11:08:38 -0300

    Add métodos e constantes para lidar com a criação de arquivos e organizar arquivos .db em db_files

--- Controlador.java
+++ Controlador.java
@@ -1,18 +1,33 @@
 package trabalho_aed_prontuario.main;
 
 import trabalho_aed_prontuario.indice.*;
-import trabalho_aed_prontuario.indice.StatusDeInsercao;
 import trabalho_aed_prontuario.mestre.*;
-import trabalho_aed_prontuario.mestre.ArquivoMestre;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Controlador {
-    private final Indice indice;
-    private final ArquivoMestre arquivo_mestre;
+    private Indice indice;
+    private ArquivoMestre arquivo_mestre;
     private static final int DEFAULT_P_GLOBAL = 1;
     private static final int DEFAULT_TAM_BUCKET = 10;
-    private static final int DEFAULT_TAM_ANOTACOES = 100;
+    private static final short DEFAULT_TAM_ANOTACOES = 100;
+    private static final String DB_FILES_DIR_NAME = "db_files";
+    private static final String INDICE_FILENAME = Paths.get(DB_FILES_DIR_NAME,"indice.db").toString();
+    private static final String DIR_FILENAME = Paths.get(DB_FILES_DIR_NAME, "diretorio.db").toString();
+    private static final String MASTER_FILENAME = Paths.get(DB_FILES_DIR_NAME, "arquivo_mestre.db").toString();
 
-    public Controlador(int profundidade_global, int tam_bucket, int tam_anotacoes) {
+    public Controlador() {
+        criarDiretorioDB();
+        // instancia o índice e o arquivo mestre com os valores
+        // default, que serão ignorados caso os arquivos já existam
+        indice = new Indice(INDICE_FILENAME, DIR_FILENAME, DEFAULT_P_GLOBAL, DEFAULT_TAM_BUCKET);
+        arquivo_mestre = new ArquivoMestre(MASTER_FILENAME, DEFAULT_TAM_ANOTACOES);
+    }
+
+    public void criarArquivos(int profundidade_global, int tam_bucket, int tam_anotacoes) {
         if (profundidade_global < 0) {
             System.out.println("Profundidade global " + profundidade_global + " inválida, assumindo valor " + DEFAULT_P_GLOBAL);
             profundidade_global = DEFAULT_P_GLOBAL;
@@ -25,8 +40,32 @@ public class Controlador {
             System.out.println("Tamanho " + tam_anotacoes + " para as anotações inválido, assumindo valor " + DEFAULT_TAM_ANOTACOES);
             tam_anotacoes = DEFAULT_TAM_ANOTACOES;
         }
-        indice = new Indice(profundidade_global, tam_bucket);
-        arquivo_mestre = new ArquivoMestre((short)tam_anotacoes);
+        apagarArquivos();
+        indice = new Indice(INDICE_FILENAME, DIR_FILENAME, profundidade_global, tam_bucket);
+        arquivo_mestre = new ArquivoMestre(MASTER_FILENAME, (short) tam_anotacoes);
+    }
+
+    // criar diretorio DB_FILES_DIR_NAME vazio se não existir;
+    private void criarDiretorioDB() {
+        try {
+            Path path = Path.of(DB_FILES_DIR_NAME);
+            if (Files.notExists(path)) {
+                Files.createDirectory(path);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // apaga os arquivos dentro de DB_FILES_DIR_NAME caso existirem
+    private void apagarArquivos() {
+        try {
+            Files.deleteIfExists(Path.of(INDICE_FILENAME));
+            Files.deleteIfExists(Path.of(DIR_FILENAME));
+            Files.deleteIfExists(Path.of(MASTER_FILENAME));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean inserirRegistro(Prontuario prontuario) {
