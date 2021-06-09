22f7a12c34e4b982fa4e2ccd17c8fbd290219a3d
natan-alex <natancarmo321@gmail.com>
2021-06-09 20:23:29 -0300

    Remover num_opcao associado aos itens do enum

--- StatusDeEdicao.java
+++ StatusDeEdicao.java
@@ -6,17 +6,7 @@ package trabalho_aed_prontuario.main;
 // campo que se deseja alterar não for válida;
 // TUDO_OK acontece quando a edição puder ser feita;
 public enum StatusDeEdicao {
-    CPF_INVALIDO(0),
-    CAMPO_A_ALTERAR_INVALIDO(1),
-    TUDO_OK(2);
-
-    int num_opcao;
-
-    private StatusDeEdicao(int num_opcao) {
-        this.num_opcao = num_opcao;
-    }
-
-    public int getNumOpcao() {
-        return num_opcao;
-    }
+    CPF_INVALIDO,
+    CAMPO_A_ALTERAR_INVALIDO,
+    TUDO_OK
 }
